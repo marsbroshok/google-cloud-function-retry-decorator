782078719136421dd396fe066179fb95f16fd8d3
Alexander Usoltsev <marsbroshok@gmail.com>
2019-07-23 17:36:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
+with open('requirements.txt') as file_requirements:
+    requirements = file_requirements.read().splitlines()
+    
 setup(
     name='Retry Decorator for Google Cloud Function',
     version='0.1',
     packages=['.',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=requirements,
 )
