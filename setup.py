9523566ae5e462a30645665b70f82834f4bc6337
Alexander Usoltsev <marsbroshok@gmail.com>
2019-07-23 17:29:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Google Cloud Function Retry Decorator',
+    version='0.1',
+    packages=['.',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
